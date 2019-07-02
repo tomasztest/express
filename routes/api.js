01ebfed4ed23204230b92a150b0399ac398dac93
tomasztest <tomaszkotzur@gmail.com>
2019-07-02 13:24:13 +0200

    api news

---
+++ routes/api.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const News = require('../models/news');
+const defaultSort = -1;
+
+router.get('/', (req, res) => {
+  const search = req.query.search || '';
+  let sort = req.query.search || defaultSort;
+
+  if (sort !== -1 || sort !== 1) {
+    sort = defaultSort;
+  }
+
+  const findNews = News
+    .find({
+      title: new RegExp(search.trim(), 'i')
+    })
+    .sort({
+      created: sort
+    })
+    .select('_id title description');
+
+  findNews.exec((err, data) => {
+    res.json(data);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  const findNews = News
+    .findById(id)
+    .select('_id title description');
+
+  findNews.exec((err, data) => {
+    res.json(data);
+  });
+});
+
+module.exports = router;
